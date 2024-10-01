0650fcbf53f3a9b033284a97376b298a35eb149b
saadsgh123 <sdsghouri@gmail.com>
2024-10-01 10:59:40 +0100

    done ✅

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
-    result = []  # This will store the final list of lists (rows of Pascal's triangle)
+"""Triangle Triangle"""
 
-    # Base case: if n is 0, return an empty list
+
+def pascal_triangle(n):
+    """ pascal_triangle function """
+    result = []
     if n <= 0:
         return result
 
-    # Generate Pascal's Triangle row by row
     for row in range(n):
-        child_list = [1] * (row + 1)  # Start each row with 1's
+        child_list = [1] * (row + 1)
 
-        # Fill the middle values by adding the two numbers from the previous row
         for i in range(1, row):
             child_list[i] = result[row - 1][i - 1] + result[row - 1][i]
 
-        result.append(child_list)  # Append the current row to the result list
+        result.append(child_list)
 
     return result
