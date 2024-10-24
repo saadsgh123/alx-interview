70f4c35c5ed6760366fdd974fed6213f2f2545d2
saadsgh123 <sdsghouri@gmail.com>
2024-10-24 19:19:28 +0100

    done ✅

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -82,7 +82,3 @@ def run():
                 print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
         print_statistics(total_file_size, status_codes_stats)
-
-
-if __name__ == '__main__':
-    run()
