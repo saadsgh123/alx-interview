16d2891bffd91e0557924a16c4e70f149ee716bc
saadsgh123 <sdsghouri@gmail.com>
2024-12-05 22:26:54 +0100

    done ✅

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,7 +14,6 @@ def island_perimeter(grid):
         for c in range(cols):
             if grid[r][c] == 1:
                 perimeter += 4
-                d
                 if r > 0 and grid[r - 1][c] == 1:
                     perimeter -= 2
                 if c > 0 and grid[r][c - 1] == 1:
